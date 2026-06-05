edd0f0fe2f578056efb96ff669a4f03e016ca7e2
Edson Santos <edson@actuar.com>
2026-06-05 16:41:40 -0300

    feat: add `GenericFetch` class and remove `Serialize` method from `GenericAction`

--- src/Toletus.LiteNet3.Handler/Requests/Actions/GenericAction.cs
+++ src/Toletus.LiteNet3.Handler/Requests/Actions/GenericAction.cs
@@ -7,18 +7,9 @@ public class GenericAction : ActionBase
     public GenericAction(string action, object? data = null)
     {
         if (string.IsNullOrWhiteSpace(action))
-        {
             throw new ArgumentException("A ação não pode ser vazia.", nameof(action));
-        }
 
         Action = action;
         Data = data;
-    }
-
-    public override string Serialize()
-    {
-        return Data is null
-            ? JsonSerializer.Serialize(new { action = Action })
-            : JsonSerializer.Serialize(new { action = Action, data = Data });
     }
 }
